538c7c86d2519c48a9c01568fdbe471efca2d6c3
Nandanigupta113 <nandanigupta1132003@gmail.com>
2024-12-31 11:07:30 +0530

    initialised

---
+++ src/main/java/com/nandini/customerordermanagement/CustomerOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nandini.customerordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOrderManagementApplication.class, args);
+	}
+
+}
